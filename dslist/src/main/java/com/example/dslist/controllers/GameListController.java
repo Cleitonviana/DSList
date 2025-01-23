47f284dafbaf3df88e36f64e06f0f576cffd1ee6
Cleitonviana <cleiton_inacio@outlook.com>
2025-01-23 16:34:12 -0300

    move games list

--- dslist/src/main/java/com/example/dslist/controllers/GameListController.java
+++ dslist/src/main/java/com/example/dslist/controllers/GameListController.java
@@ -5,13 +5,18 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.dslist.dto.GameListDTO;
 import com.example.dslist.dto.GameMinDTO;
+import com.example.dslist.dto.ReplacementDTO;
 import com.example.dslist.services.GameListService;
 import com.example.dslist.services.GameService;
+
+import jakarta.annotation.PostConstruct;
 
 @RestController
 @RequestMapping(value = "/lists")
@@ -32,6 +37,11 @@ public class GameListController {
 	public List<GameMinDTO> FindByList(@PathVariable Long listId){
 		List<GameMinDTO> result = gameService.findByList(listId);
 		return result;
+	}
+	
+	@PostMapping(value = "/{listId}/replacement")
+	public void move(@PathVariable Long listId, @RequestBody ReplacementDTO body ){
+		gameListService.move(listId, body.getSourceIndex(), body.getDestinationIndex());
 	}
 	
 }
---
+++ dslist/src/main/java/com/example/dslist/dto/ReplacementDTO.java
@@ -0,0 +1,24 @@
+package com.example.dslist.dto;
+
+public class ReplacementDTO {
+
+	Integer sourceIndex;
+	Integer destinationIndex;
+
+	public Integer getSourceIndex() {
+		return sourceIndex;
+	}
+
+	public void setSourceIndex(Integer sourceIndex) {
+		this.sourceIndex = sourceIndex;
+	}
+
+	public Integer getDestinationIndex() {
+		return destinationIndex;
+	}
+
+	public void setDestinationIndex(Integer destinationIndex) {
+		this.destinationIndex = destinationIndex;
+	}
+
+}
